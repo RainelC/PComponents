84d306b062a8c45451a71cf6cbcdb50fd141c4b0
RainelC <rainelmartinez13@gmail.com>
2024-12-06 23:17:38 -0400

    fixing img

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const router = require('../routers/router');
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const router = require("../routers/router");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,32 +12,36 @@ const PORT = process.env.PORT || 4000;
 
 mongoose.connect(process.env.DB_URI);
 const db = mongoose.connection;
-db.on('error', (error) => console.log(error));
-db.once('open', () => console.log("Connection ready"));
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connection ready"));
 
 // Middleware
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(session({
-    secret: 'popa',
+app.use(
+  session({
+    secret: "popa",
     saveUninitialized: true,
-    resave: false
-}))
+    resave: false,
+  })
+);
 
 app.use((req, res, next) => {
-    res.locals.massage = req.session.massage;
-    delete req.session.massage;
-    next();
-})
+  res.locals.massage = req.session.massage;
+  delete req.session.massage;
+  next();
+});
 
-/// View Engine 
-app.set('view engine', 'ejs');
-app.use('', router);
+app.use(express.static("upload"));
+
+/// View Engine
+app.set("view engine", "ejs");
+app.use("", router);
 
 /////////////////
 
 app.listen(PORT, () => {
-    console.log(`Server http://localhost:${PORT}`);
+  console.log(`Server http://localhost:${PORT}`);
 });
